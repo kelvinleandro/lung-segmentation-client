0d36778e5dd8baeba0cde1a53584faf3969ad77d
kelvinleandro <kelvinleandro5@gmail.com>
2025-02-02 17:20:33 -0300

    modified: mudado maneira de enviar arquivo para API (falta testar)

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,11 @@
 import { useState, useRef, useEffect } from "react";
 import axios from "axios";
-import * as cornerstone from "@cornerstonejs/core";
-import * as cornerstoneDICOMImageLoader from "@cornerstonejs/dicom-image-loader";
+import { init as coreInit, RenderingEngine, imageLoader, Types, Enums} from "@cornerstonejs/core";
+import {init as dicomImageLoaderInit, wadouri} from "@cornerstonejs/dicom-image-loader";
 import { Upload } from "lucide-react";
 
-cornerstone.init();
-cornerstoneDICOMImageLoader.init();
+coreInit();
+dicomImageLoaderInit();
 
 const renderingEngineId = "myRenderingEngine";
 const viewportId = "dicomViewport";
@@ -14,7 +14,7 @@ const HomePage = () => {
   const [image, setImage] = useState<string | null>(null);
   const [dicomFile, setDicomFile] = useState<File | null>(null);
   const imageRef = useRef<HTMLDivElement>(null);
-  const renderingEngineRef = useRef<cornerstone.RenderingEngine | null>(null);
+  const renderingEngineRef = useRef<RenderingEngine | null>(null);
 
   useEffect(() => {
     return () => {
@@ -32,12 +32,12 @@ const HomePage = () => {
 
     setDicomFile(file);
 
-    const imageId = cornerstoneDICOMImageLoader.wadouri.fileManager.add(file);
+    const imageId = wadouri.fileManager.add(file);
     console.log("ID da imagem:", imageId);
     setImage(imageId);
 
     try {
-      const _image = await cornerstone.imageLoader.loadImage(imageId);
+      const _image = await imageLoader.loadImage(imageId);
       console.log("Imagem carregada:", _image);
       
 
@@ -47,46 +47,57 @@ const HomePage = () => {
       }
 
       if (!renderingEngineRef.current) {
-        renderingEngineRef.current = new cornerstone.RenderingEngine(renderingEngineId);
+        renderingEngineRef.current = new RenderingEngine(renderingEngineId);
       }
 
       const renderingEngine = renderingEngineRef.current;
 
       const viewportInput = {
         viewportId,
-        type: cornerstone.Enums.ViewportType.STACK,
+        type: Enums.ViewportType.STACK,
         element: imageRef.current,
       }
 
       renderingEngine.enableElement(viewportInput);
-      const viewport = renderingEngine.getViewport(viewportId) as cornerstone.StackViewport;
-      viewport.setStack([imageId]);
+      const viewport = renderingEngine.getViewport(viewportId) as Types.IStackViewport;
+      await viewport.setStack([imageId]);
       viewport.render();
 
       setTimeout(function(){ const metadata = viewport.getImageData(); console.log('Metadados:', metadata);  }, 5000);
 
     } catch (error) {
-      console.error("Error loading DICOM image:", error);
+      console.error("Erro ao carregar imagem DICOM:", error);
     }
   };
 
   const sendFileToServer = async () => {
     if (!dicomFile) return;
+    const formData = new FormData();
+    formData.append("dicom", dicomFile);
 
-    const reader = new FileReader();
-    reader.readAsDataURL(dicomFile);
-    reader.onload = async () => {
-      const base64 = reader.result as string;
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/upload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+        responseType: "blob", // Expecting a binary response (DICOM file)
+      });
 
-      try {
-        const response = await axios.post("/api/upload-dicom", {
-          file: base64,
-        });
-        console.log("File uploaded successfully:", response.data);
-      } catch (error) {
-        console.error("Error uploading file:", error);
-      }
-    };
+      console.log("Resposta do servidor:", response.data);
+
+      // const blob = new Blob([response.data], { type: "application/dicom" });
+      // const file = new File([blob], "rotated.dcm");
+
+      // const imageId = wadouri.fileManager.add(file);
+      // setImage(imageId);
+
+      // const renderingEngine = renderingEngineRef.current;
+      // if (renderingEngine) {
+      //   const viewport = renderingEngine.getViewport(viewportId) as Types.IStackViewport;
+      //   await viewport.setStack([imageId]);
+      //   viewport.render();
+      // }
+    } catch (error) {
+      console.error("Erro ao enviar arquivo DICOM:", error);
+    }
   };
 
   return (
