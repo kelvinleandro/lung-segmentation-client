906504d113a20a98ee2780361facd54f6571a898
Adauta <adautacostaa.freire@gmail.com>
2025-02-15 17:53:00 -0300

    feat 18: adiciona ThemeContext para mudar o tema entre dark/light

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ApiProvider } from './context/api-context.tsx'
 import './index.css'
+import { ThemeProvider } from "./context/theme-context";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApiProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </ApiProvider>
   </React.StrictMode>,
 )
