9d18300b6632871060434bcc7c98f5390dd17be3
fasnfilho <fabinhosnf@gmail.com>
2025-02-24 20:44:23 -0300

    Criando constantes para uso na sidebar

---
+++ src/constants/segmentationDefaults.ts
@@ -0,0 +1,13 @@
+export const segmentationDefaultValues = {
+    CrispParameters: {
+        threshold: 0.5,
+        smoothness: 1.0,
+    },
+    OtsuParameters: {
+        bins: 256,
+    },
+    WatershedParameters: {
+        markers: 10,
+        compactness: 0.01,
+    },
+};
